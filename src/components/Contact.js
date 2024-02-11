094f8e4ee037cadb41730c72c64abe3ce57b6f9f
Hung-Ta-Chen <htcchen@ucdavis.edu>
2024-02-11 04:56:08 -0800

    Add contact feature

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "../styles/Contact.css";
+
 function Contact() {
   return (
     <section id="contact" className="contact last-section">
       <h1 className="section-title">Contact Me</h1>
       <form className="contact-form">
-        <input type="text" name="name" placeholder="Your Name" required />
-        <input type="email" name="email" placeholder="Your Email" required />
-        <textarea name="message" placeholder="Your Message" required></textarea>
-        <button type="submit">Send Message</button>
+        <form
+          className="contact-form"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+        >
+          {/* The following hidden input is required for Netlify forms to work with React */}
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit">Send Message</button>
+        </form>
       </form>
     </section>
   );
