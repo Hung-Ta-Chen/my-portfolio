82d2bae499d16045e26ae806f6d854d67a2d359c
Hung-Ta-Chen <htcchen@ucdavis.edu>
2024-02-11 05:21:50 -0800

    Fix the multiline errors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,10 +6,10 @@ const projects = [
     id: 1,
     title: "My Portfolio Website",
     description:
-      "This project is a personal portfolio website meticulously crafted using React and CSS, \
-      showcasing a seamless blend of functionality and aesthetics. It serves as a digital showcase, \
-      featuring an 'About Me' narrative, a curated gallery of projects, sections on skills, \
-      a curated gallery of projects, and a contact form for easy outreach.",
+      "This project is a personal portfolio website meticulously crafted using React and CSS, " +
+      "showcasing a seamless blend of functionality and aesthetics. It serves as a digital showcase, " +
+      "featuring an 'About Me' narrative, a curated gallery of projects, sections on skills, " +
+      "a curated gallery of projects, and a contact form for easy outreach.",
     technologies: "React.js, CSS",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/my-portfolio",
@@ -18,10 +18,10 @@ const projects = [
     id: 2,
     title: "Event Management API",
     description:
-      "A NestJS-based Event Management API project showcasing CRUD operations, \
-      event merging capabilities, and database integration using Prisma ORM with PostgreSQL. \
-      Highlights include RESTful API design, comprehensive Jest testing, and Docker deployment, \
-      illustrating advanced backend development techniques and practices.",
+      "A NestJS-based Event Management API project showcasing CRUD operations, " +
+      "event merging capabilities, and database integration using Prisma ORM with PostgreSQL. " +
+      "Highlights include RESTful API design, comprehensive Jest testing, and Docker deployment, " +
+      "illustrating advanced backend development techniques and practices.",
     technologies: "NestJS, PostgreSQL, Docker",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/event-management-API",
@@ -30,11 +30,11 @@ const projects = [
     id: 3,
     title: "Food Waste Reduction App",
     description:
-      "This project involved the development of a food recognition mobile application,\
-      featuring a React Native frontend and a Django backend API. With an integration of Clarifai’s recognition model, \
-      the backend API was responsible for handling various tasks such as image processing, classification, and data retrieval. \
-      By implementing robust API endpoints, the project ensured seamless communication between the frontend and backend systems, \
-      resulting in a user-friendly and responsive application experience.",
+      "This project involved the development of a food recognition mobile application, " +
+      "featuring a React Native frontend and a Django backend API. With an integration of Clarifai’s recognition model, " +
+      "the backend API was responsible for handling various tasks such as image processing, classification, and data retrieval. " +
+      "By implementing robust API endpoints, the project ensured seamless communication between the frontend and backend systems, " +
+      "resulting in a user-friendly and responsive application experience.",
     technologies: "React Native, Django, SQLite",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/",
@@ -43,10 +43,10 @@ const projects = [
     id: 4,
     title: "Job Application Tracker",
     description:
-      "Developed using C# within Visual Studio, this application serves as an efficient tool for job seekers to manage \
-      and track their applications throughout their job search journey. Designed with a user-friendly interface, \
-      it allows users to add and count their applications in real-time. This project highlights my skills in software development, \
-      problem-solving, and user experience design, offering a practical solution to streamline the job application process.",
+      "Developed using C# within Visual Studio, this application serves as an efficient tool for job seekers to manage " +
+      "and track their applications throughout their job search journey. Designed with a user-friendly interface, " +
+      "it allows users to add and count their applications in real-time. This project highlights my skills in software development, " +
+      "problem-solving, and user experience design, offering a practical solution to streamline the job application process.",
     technologies: "C#, .NET framework",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/Job-Application-Counter",
@@ -55,10 +55,10 @@ const projects = [
     id: 6,
     title: "AI-Powered Coding Problem Classifier",
     description:
-      "This project harnesses advanced natural language processing techniques to predict LeetCode problem topics from their descriptions, \
-      utilizing BERT embeddings for accurate categorization through multi-label classification. A comprehensive dataset of over 2500+ coding problems, \
-      scraped from various coding websites and stored in a MySQL database, underpins the model's training, \
-      ensuring a broad and effective problem-solving tool for efficient navigation and challenge tackling in the coding community.",
+      "This project harnesses advanced natural language processing techniques to predict LeetCode problem topics from their descriptions, " +
+      "utilizing BERT embeddings for accurate categorization through multi-label classification. A comprehensive dataset of over 2500+ coding problems, " +
+      "scraped from various coding websites and stored in a MySQL database, underpins the model's training, " +
+      "ensuring a broad and effective problem-solving tool for efficient navigation and challenge tackling in the coding community.",
     technologies: "Python, PyTorch, MySQL",
     image: "",
     github:
@@ -68,11 +68,11 @@ const projects = [
     id: 7,
     title: "English Vocabulary Learning App",
     description:
-      "This application, developed with JavaFX and utilizing MVC architecture, \
-      offers users an engaging platform to enhance their English vocabulary. \
-      It features an interactive user interface and leverages Java Database Connectivity (JDBC) for efficient data management. \
-      Users can enrich their learning experience by adding flashcards and testing their knowledge through quizzes, \
-      making vocabulary building both effective and enjoyable.",
+      "This application, developed with JavaFX and utilizing MVC architecture, " +
+      "offers users an engaging platform to enhance their English vocabulary. " +
+      "It features an interactive user interface and leverages Java Database Connectivity (JDBC) for efficient data management. " +
+      "Users can enrich their learning experience by adding flashcards and testing their knowledge through quizzes, " +
+      "making vocabulary building both effective and enjoyable.",
     technologies: "Java, JavaFX, MySQL",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/English-Vocabulary-Learning-App",
@@ -81,10 +81,10 @@ const projects = [
     id: 8,
     title: "UAV Object Detection System",
     description:
-      "Leveraged YOLOv5 and OpenCV to create a cutting-edge object detection system for UAVs, \
-      tailored for optimal performance across various conditions. \
-      This project also integrates a robust UDP protocol for seamless UAV remote control and efficient image streaming, \
-      enhancing operational efficiency and real-time monitoring capabilities.",
+      "Leveraged YOLOv5 and OpenCV to create a cutting-edge object detection system for UAVs, " +
+      "tailored for optimal performance across various conditions. " +
+      "This project also integrates a robust UDP protocol for seamless UAV remote control and efficient image streaming, " +
+      "enhancing operational efficiency and real-time monitoring capabilities.",
     technologies: "Python, PyTorch",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/UAV-object-detection-with-yolov3",
@@ -93,9 +93,9 @@ const projects = [
     id: 9,
     title: "ROS-Based Trail Classification System",
     description:
-      "The project developed a Convolutional Neural Network model in PyTorch for accurate trail classification, \
-      seamlessly integrated with the Robot Operating System (ROS) for dynamic, real-time image analysis and robotic adjustments. \
-      The system enhances autonomous navigation by enabling robots to intelligently recognize and adjust to different trail environments.",
+      "The project developed a Convolutional Neural Network model in PyTorch for accurate trail classification, " +
+      "seamlessly integrated with the Robot Operating System (ROS) for dynamic, real-time image analysis and robotic adjustments. " +
+      "The system enhances autonomous navigation by enabling robots to intelligently recognize and adjust to different trail environments.",
     technologies: "Python, PyTorch, ROS",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/Lane-Following-Robot",
@@ -104,9 +104,9 @@ const projects = [
     id: 10,
     title: "Real-Time Chatroom Application",
     description:
-      "This project features a real-time chatroom server developed with JavaScript and Socket.io, \
-      complemented by a sleek client interface designed in Swift. Utilizing HTML for server-side design, \
-      the application facilitates seamless communication, offering an intuitive and responsive user experience across platforms.",
+      "This project features a real-time chatroom server developed with JavaScript and Socket.io, " +
+      "complemented by a sleek client interface designed in Swift. Utilizing HTML for server-side design, " +
+      "the application facilitates seamless communication, offering an intuitive and responsive user experience across platforms.",
     technologies: "JavaScript, Socket.io, Swift",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/Simple-Chatroom-Project",
@@ -115,10 +115,10 @@ const projects = [
     id: 11,
     title: "Employee Resignation Prediction Project",
     description:
-      "This project aims to forecast employee resignations using advanced machine learning techniques. \
-      By employing a variety of models, including Gaussian Naive Bayes, Decision Tree, Random Forest, and XGBoost, \
-      the data is meticulously analyzed. A VotingClassifier from sklearn further enhances accuracy through ensemble learning, \
-      providing reliable predictions on employee turnover.",
+      "This project aims to forecast employee resignations using advanced machine learning techniques. " +
+      "By employing a variety of models, including Gaussian Naive Bayes, Decision Tree, Random Forest, and XGBoost, " +
+      "the data is meticulously analyzed. A VotingClassifier from sklearn further enhances accuracy through ensemble learning, " +
+      "providing reliable predictions on employee turnover.",
     technologies: "Python, Scikit-learn, XGBoost",
     image: "",
     github:
@@ -128,9 +128,9 @@ const projects = [
     id: 12,
     title: '"Dad n Me" 2D Action Game Remake',
     description:
-      'A simple implementation of the classic action game "Dad n Me" using SDL 2.0 in C. \
-      This project features side-scrolling maps, precise collision detection, and responsive keyboard controls, \
-      recreating the iconic gaming experience with enhanced functionality and immersive gameplay.',
+      'A simple implementation of the classic action game "Dad n Me" using SDL 2.0 in C. ' +
+      "This project features side-scrolling maps, precise collision detection, and responsive keyboard controls, " +
+      "recreating the iconic gaming experience with enhanced functionality and immersive gameplay.",
     technologies: "C, SDL 2.0",
     image: "",
     github: "https://github.com/Hung-Ta-Chen/Action-Game-Project",
